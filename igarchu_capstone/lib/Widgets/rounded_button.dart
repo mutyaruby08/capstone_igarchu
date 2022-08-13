831ce737dffa48283dd3c200296affbfc8ea39f6
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2022-08-13 17:48:38 +0800

    Designed Login Screen ver1

---
+++ igarchu_capstone/lib/Widgets/rounded_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:igarchu_capstone/constants.dart';
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton(
+      {Key? key, required this.text, this.press, this.textColor = Colors.white})
+      : super(key: key);
+
+  final String text;
+  final Function()? press;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      width: size.width * 0.8,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: newElevatedButton(),
+      ),
+    );
+  }
+
+  Widget newElevatedButton() {
+    return ElevatedButton(
+      child: Text(
+        text,
+        style: TextStyle(color: textColor, fontSize: 17),
+      ),
+      onPressed: press,
+      style: ElevatedButton.styleFrom(
+        //di ko ka change sa color sa button, idk why
+        primary: Colors.black,
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        textStyle: TextStyle(
+          letterSpacing: 2,
+          color: textColor,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Poppins',
+        ),
+      ),
+    );
+  }
+}
---
+++ igarchu_capstone/lib/Widgets/upside.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:igarchu_capstone/constants.dart';
+// import 'package:h3f_wedding_planner/constants.dart';
+
+class Upside extends StatelessWidget {
+  const Upside({Key? key, required this.imgUrl}) : super(key: key);
+
+  final String imgUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Container(
+          width: 200.0,
+          height: 200.0,
+          color: kPrimaryLightColor,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 0.0),
+            child: Image.asset(
+              imgUrl,
+              alignment: Alignment.topCenter,
+              scale: 1,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// iconBackButton(BuildContext context) {
+//   return IconButton(
+//     color: Colors.white,
+//     iconSize: 28,
+//     icon: const Icon(CupertinoIcons.arrow_left),
+//     onPressed: () {
+//       Navigator.pop(context);
+//     },
+//   );
+
